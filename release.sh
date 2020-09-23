d308d3d99ea596805f6eb0f6afd90f2219a6d441
veluscekm <marco.veluscek@skytv.it>
2020-09-23 17:50:08 +0200

    Minor: fix release script

--- release.sh
+++ release.sh
@@ -3,28 +3,29 @@
 SCOPE="$1"
 
 # Get last commit and parse text
-if [[ -z "$SCOPE" ]]; then
+if [ -z "$SCOPE" ]; then
+    echo "Scope is empty. Trying to get from commit message..."
     SCOPE=$(git log -1 | egrep -ohi '(MAJOR|MINOR|PATCH):' | head -1 | tr '[:upper:]' '[:lower:]')
 fi
 
-if [[ -z "$SCOPE" ]]; then
+if [ -z "$SCOPE" ]; then
+  echo "Scope is still empty. Setting it to auto..."
   SCOPE="auto"
 fi
 
 echo "Using scope $SCOPE"
 
-# Get the next version, without tagging
-echo "Getting next version"
-nextversion="$(source semtag final -fos $SCOPE)"
+echo "Getting next version, without tagging"
+nextversion="$(sh semtag final -fos $SCOPE)"
 echo "Publishing with version: $nextversion"
 
-# Update the tag with the new version
-source semtag final -f -v $nextversion
+echo "Update the tag with the new version"
+sh semtag final -f -v $nextversion
 
 export PROG_VERSION=$nextversion
 
-# Build
+echo "Building"
 sh build.sh
 
-# Create release notes
+echo "Create release notes"
 git log -1 | tail -n +5 > release-notes.md
