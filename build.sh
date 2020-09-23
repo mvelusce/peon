9369b38f105af9414fb93eae44e9da73d629e619
veluscekm <marco.veluscek@skytv.it>
2020-09-23 17:37:56 +0200

    Minor: enable automatic release

--- build.sh
+++ build.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
 cd cmd/peon
-go build
+
+echo "Building for Linux..."
+env GOOS=linux GOARCH=arm go build -v -o peon-linux
+
+echo "Building for OSX..."
+env GOOS=darwin GOARCH=386 go build -v -o peon-osx
+
+echo "Building for Windows..."
+env GOOS=windows go build -v -o peon-windows
+
+echo "Zipping artifacts..."
+zip peon.zip peon-linux peon-osx peon-windows
