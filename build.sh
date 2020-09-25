05f89f01c688a3015816a99fa29a97e6e557e3b4
Marco Veluscek <43888451+skyveluscekm@users.noreply.github.com>
2020-09-25 11:16:08 +0200

    Fix linux arch (#3)

--- build.sh
+++ build.sh
@@ -7,13 +7,13 @@ envsubst < main.go > main_with_version.go
 mv main_with_version.go main.go
 
 echo "Building for Linux..."
-env GOOS=linux GOARCH=x86-64 go build -v -o peon-linux
+env GOOS=linux GOARCH=386 go build -v -o peon-linux
 
 echo "Building for OSX..."
 env GOOS=darwin GOARCH=386 go build -v -o peon-osx
 
 echo "Building for Windows..."
-env GOOS=windows go build -v -o peon-windows
+env GOOS=windows GOARCH=386 go build -v -o peon-windows
 
 echo "Zipping artifacts..."
 zip peon.zip peon-linux peon-osx peon-windows
